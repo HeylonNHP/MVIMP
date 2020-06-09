15b1917ce06b4fd327d34ff3fa0c21d86422b5b7
heylon <heylon96@hotmail.com>
2020-06-09 21:31:49 +1000

    attempted turing fix #1

--- third_party/DAIN/my_package/SeparableConvFlow/setup.py
+++ third_party/DAIN/my_package/SeparableConvFlow/setup.py
@@ -23,7 +23,7 @@ nvcc_args = [
     "-gencode",
     "arch=compute_61,code=sm_61"
     '-gencode', 'arch=compute_70,code=sm_70',
-    '-gencode', 'arch=compute_70,code=compute_70',
+    # '-gencode', 'arch=compute_70,code=compute_70',
     "-gencode", "arch=compute_75,code=sm_75"
 ]
 
