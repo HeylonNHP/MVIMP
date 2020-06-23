228b470ffcf92177a709bff73523aa757276deb3
Heylon <heylon96@hotmail.com>
2020-06-23 17:51:31 +1000

    Reconnect upon trying to change directories when disconnected Also cleaned up the code

--- auto.py
+++ auto.py
@@ -1,53 +1,50 @@
 from ftplib import FTP
 import os
+
+# Change these
 VIDEO_TO_PROCESS = "videoName"
+FTP_SERVER_ADDRESS = "mywebserver.ddns.net"
+INTERPOLATION_TIME_STEP = 0.125
+
+# Don't change these
 LOCAL_INPUT_DIR = "/content/MVIMP/Data/Input"
 LOCAL_OUTPUT_DIR = "/content/MVIMP/Data/Output"
 
-def runAndPrintOutput(command):
-    import subprocess
-    process = subprocess.Popen(command,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE,
-                               universal_newlines=True, shell=True)
-
-    while True:
-        output = process.stdout.readline()
-        error = process.stderr.readline()
-        print(output.strip())
-        print(error.strip())
-        # Do something else
-        return_code = process.poll()
-        if return_code is not None:
-            print('RETURN CODE', return_code)
-            # Process has finished, read rest of the output
-            for output in process.stdout.readlines():
-                print(output.strip())
-            break
-
-def changeDirWithRetry(ftpObj, dirpath):
-    success = False
-    while success == False:
-        try:
-            ftpObj.cwd(dirpath)
-            success = True
-        except:
-            pass
 # FTP login details - MAKE SURE TO SET: the FTP server's connection timeout to infinity
-ftp = FTP('mywebserver.ddns.net')
-ftp.login(user="collab",passwd='collab')
+ftp = None
+def connectFtp():
+    global ftp
+    ftp = FTP(FTP_SERVER_ADDRESS)
+    ftp.login(user="collab",passwd='collab')
 
+
+connectFtp()
+# Go into input dir
 files = ftp.nlst()
 for file in files:
     if VIDEO_TO_PROCESS == file:
         ftp.cwd(file)
 
 print(ftp.pwd())
-
-# Go into input dir
 ftp.cwd("in")
-
 print(ftp.pwd())
+
+def changeDirWithRetry(dirpath):
+    global ftp
+    success = False
+    while success == False:
+        try:
+            ftp.cwd(dirpath)
+            success = True
+        except:
+            try:
+                prevWorkingDir = ftp.pwd()
+                ftp.close()
+                connectFtp()
+                ftp.cwd(prevWorkingDir)
+            except:
+                pass
+
 
 # For each input file in the input folder
 for inputfile in ftp.nlst():
@@ -58,25 +55,15 @@ for inputfile in ftp.nlst():
     ftp.retrbinary('RETR %s' % ftp.pwd() + "/" + inputfile, fileObject.write)
     fileObject.close()
     # Run interpolation
-    os.system('python3 inference_dain.py --input_video "' + inputfile + '" --time_step 0.125')
+    os.system('python3 inference_dain.py --input_video "' + inputfile + '" --time_step ' + INTERPOLATION_TIME_STEP)
     # Grab output
-    changeDirWithRetry(ftp,"..")
-    changeDirWithRetry(ftp, "out")
+    changeDirWithRetry("..")
+    changeDirWithRetry("out")
     for (dirpath, dirnames, filenames) in os.walk(LOCAL_OUTPUT_DIR):
         outputFilePath = dirpath + "/" + filenames[0]
-        ftp.storbinary('STOR ' + filenames[0], open(outputFilePath, 'rb'))
+        outputFile = open(outputFilePath, 'rb')
+        ftp.storbinary('STOR ' + filenames[0], outputFile)
+        outputFile.close()
         os.remove(outputFilePath)
-    changeDirWithRetry(ftp, "..")
-    changeDirWithRetry(ftp, "in")
-
-
-
-'''
-files = []
-ftp.dir(files.append)
-
-for file in files:
-    print(type(file))
-    print(file)
-
-'''
+    changeDirWithRetry("..")
+    changeDirWithRetry("in")
